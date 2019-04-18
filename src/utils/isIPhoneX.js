267763a33fbcc4bc29ca8fccb430a338ac82c79a
Jordan Koncz <jordanmkoncz@gmail.com>
2019-04-18 14:38:39 +1000

    Update isIPhoneX to handle iPhone XS Max.

--- src/utils/isIPhoneX.js
+++ src/utils/isIPhoneX.js
@@ -1,4 +1,16 @@
-import { Platform, Dimensions } from 'react-native';
-const { width, height } = Dimensions.get('window');
-const isIPhoneX = Platform.OS === 'ios' && width === 375 && height === 812;
+import { Dimensions, Platform } from 'react-native';
+
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+const XSMAX_WIDTH = 414;
+const XSMAX_HEIGHT = 896;
+
+const { height: D_HEIGHT, width: D_WIDTH } = Dimensions.get('window');
+
+// See node_modules/react-native-safe-area-view/index.js.
+const isIPhoneX = (() =>
+  (Platform.OS === 'ios' &&
+    ((D_HEIGHT === X_HEIGHT && D_WIDTH === X_WIDTH) || (D_HEIGHT === X_WIDTH && D_WIDTH === X_HEIGHT))) ||
+  ((D_HEIGHT === XSMAX_HEIGHT && D_WIDTH === XSMAX_WIDTH) || (D_HEIGHT === XSMAX_WIDTH && D_WIDTH === XSMAX_HEIGHT)))();
+
 export default isIPhoneX;
